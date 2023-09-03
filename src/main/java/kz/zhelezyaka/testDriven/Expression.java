e87d20132e889f4e0977b6e778be0e48669644d7
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-09-03 15:37:34 +0300

    JUnit Maven Project Setup

---
+++ src/main/java/kz/zhelezyaka/testDriven/Expression.java
@@ -0,0 +1,9 @@
+package kz.zhelezyaka.testDriven;
+
+public interface Expression {
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int i);
+}
