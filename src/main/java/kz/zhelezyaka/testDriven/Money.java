3b321c1a386d48e1978eefc86786d5d7f872c9bf
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-09-03 19:30:02 +0300

    Added some files in repository

---
+++ src/main/java/kz/zhelezyaka/testDriven/Money.java
@@ -0,0 +1,54 @@
+package kz.zhelezyaka.testDriven;
+
+import java.util.Objects;
+
+public class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "FRK");
+    }
+
+    public Expression times(int i) {
+        return new Money(amount * i, this.currency);
+    }
+
+    @Override
+    public Money reduce(Bank bank, String to) {
+        return new Money(amount / bank.rate(this.currency, to), to);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount
+                && Objects.equals(this.currency, money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
