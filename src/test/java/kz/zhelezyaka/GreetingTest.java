11b1662e5480b59396b41f4c2aa5822df4c3e82d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-09-04 18:29:08 +0300

    Initializing Properties with JUnit 5 @BeforeEach

--- src/test/java/kz/zhelezyaka/GreetingTest.java
+++ src/test/java/kz/zhelezyaka/GreetingTest.java
@@ -1,18 +1,24 @@
 package kz.zhelezyaka;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+   private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each methods...");
+        greeting = new Greeting();
+    }
 
     @Test
     void helloTDD() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloTDD());
     }
 
     @Test
     void testHelloTDD() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloTDD("Vladimir from TDD"));
     }
 }
